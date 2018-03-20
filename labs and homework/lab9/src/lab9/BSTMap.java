278b9a11b803929dd7524eba111cb7c9af898e12
unknown <zinizhu@berkeley.edu>
2018-03-19 22:40:07 -0700

    Lab9

---
+++ labs and homework/lab9/src/lab9/BSTMap.java
@@ -0,0 +1,194 @@
+package lab9;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Implementation of interface Map61B with BST as core data structure.
+ *
+ * @author Zini Zhu
+ */
+public class BSTMap<K extends Comparable<K>, V> implements lab9.Map61B<K, V> {
+
+    private class Node {
+        /* (K, V) pair stored in this Node. */
+        private K key;
+        private V value;
+
+
+        /* Children of this Node. */
+        private Node left;
+        private Node right;
+
+        private Node(K k, V v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private Node root;  /* Root node of the tree. */
+    private int size; /* The number of key-value pairs in the tree */
+    private Set<K> keySets;/* Store all keys in the map.*/
+
+    /* Creates an empty BSTMap. */
+    public BSTMap() {
+        this.clear();
+    }
+
+    /* Removes all of the mappings from this map. */
+    @Override
+    public void clear() {
+        root = null;
+        size = 0;
+        keySets = new HashSet<K>();
+    }
+
+    /** Returns the value mapped to by KEY in the subtree rooted in P.
+     *  or null if this map contains no mapping for the key.
+     */
+    private V getHelper(K key, Node p) {
+        if (p == null) {
+            return null;
+        }
+        int cmp = key.compareTo(p.key);
+        if (cmp > 0) {
+            return getHelper(key, p.right);
+        }
+        else if (cmp < 0) {
+            return getHelper(key, p.left);
+        }
+        else {
+            return p.value;
+        }
+    }
+
+    /** Returns the value to which the specified key is mapped, or null if this
+     *  map contains no mapping for the key.
+     */
+    @Override
+    public V get(K key) {
+        return getHelper(key,root);
+    }
+
+    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
+      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+     */
+    private Node putHelper(K key, V value, Node p) {
+        if (p == null) {
+            size += 1;
+            keySets.add(key);
+            return new Node(key,value);
+        }
+        int cmp = key.compareTo(p.key);
+        if (cmp > 0) {
+            return putHelper(key,value,p.right);
+        }
+        else if (cmp < 0) {
+            return putHelper(key,value,p.left);
+        }
+        else {
+            p.value = value;
+            return p;
+        }
+    }
+
+    /** Inserts the key KEY
+     *  If it is already present, updates value to be VALUE.
+     */
+    @Override
+    public void put(K key, V value) {
+        root = putHelper(key,value,root);
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
+
+    /* Returns a Set view of the keys contained in this map. */
+    @Override
+    public Set<K> keySet() {
+        return keySets;
+    }
+
+    /** Removes KEY from the tree if present
+     *  returns VALUE removed,
+     *  null on failed removal.
+     */
+    @Override
+    public V remove(K key) {
+        if (!keySets.contains(key)) {
+            throw new NullPointerException("There is no such key in the map.");
+        }
+        V value = get(key);
+        remove(root,key);
+        return value;
+    }
+
+    private Node remove(Node p, K key) {
+        if (p == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) {
+            p.left = remove(p.right,key);
+        }
+        else if (cmp > 0) {
+            p.right = remove(p.left,key);
+        }
+        else {
+            if (p.right == null) {
+                return p.left;
+            }
+            if (p.left == null) {
+                return p.right;
+            }
+            Node t = p;
+            p = min(t.right);
+            p.right = deleteMin(t.right);
+            p.left = t.left;
+        }
+        keySets.remove(key);
+        size -= 1;
+        return p;
+
+    }
+
+    private Node min(Node p) {
+        if (p.left == null ) {
+            return p;
+        }
+        return min(p.left);
+    }
+
+    private Node deleteMin(Node p) {
+        if (p.left == null) {
+            return p.right;
+        }
+        return deleteMin(p.left);
+    }
+
+    /** Removes the key-value entry for the specified key only if it is
+     *  currently mapped to the specified value.  Returns the VALUE removed,
+     *  null on failed removal.
+     **/
+    @Override
+    public V remove(K key, V value) {
+        V val = get(key);
+        if (!val.equals(value)) {
+            throw new NullPointerException("Value " + value + " is not linked to this key");
+        }
+        put(key,null);
+        return value;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
